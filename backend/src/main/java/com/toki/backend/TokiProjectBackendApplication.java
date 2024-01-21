f6a0fcdd709aeaf9f6c7ecfc4cd22218e7fdd07b
mudokja <mudokja@gmail.com>
2024-01-21 16:27:39 +0900

    init: initial commit for backend
    초기 프로젝트 커밋, 샘플용으로 작성한 소셜로그인 코드가 생각보다 잘 작동해서 환경변수 제외 후 옮김
    
    Signed-off-by: mudokja <mudokja@gmail.com>

---
+++ backend/src/main/java/com/toki/backend/TokiProjectBackendApplication.java
@@ -0,0 +1,19 @@
+package com.toki.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = {"com.toki.backend.db.repository"})
+@EntityScan(basePackages = {"com.toki.backend.DB.entity"})
+@EnableJpaAuditing
+@SpringBootApplication
+public class TokiProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokiProjectBackendApplication.class, args);
+	}
+
+}
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div>
+      기본 생성 테스트 프로젝트
+      <a href="http://localhost:8081/oauth2/authorization/naver">네이버 로그인 테스트</a>
+      <a href="http://localhost:8081/oauth2/authorization/kakao">카카오 로그인 테스트</a>
+    </div>
+  </main>
+</template>
