2d34739c76ad242c3023864b7d0c4e5101730d39
seongmin <pok_tan@naver.com>
2024-01-24 16:54:15 +0900

    feat:friend controller, dto, service

---
+++ backend/src/main/java/com/toki/backend/friend/dto/FriendResponseDto.java
@@ -0,0 +1,14 @@
+package com.toki.backend.friend.dto;
+
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class FriendResponseDto {
+
+    private String friendId;
+
+    private String done = "done";
+
+
+}
