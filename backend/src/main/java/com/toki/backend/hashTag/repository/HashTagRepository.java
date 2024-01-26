e88fcb69e38d5f9995c49c1bd09d2ff494b201f3
geunhyeongKim <ppoyam_2@naver.com>
2024-01-26 10:01:05 +0900

    feat:bannerCreate

--- backend/src/main/java/com/toki/backend/hashTag/repository/HashTagRepository.java
+++ backend/src/main/java/com/toki/backend/hashTag/repository/HashTagRepository.java
@@ -5,6 +5,7 @@ import net.minidev.json.JSONUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,10 +13,14 @@ public interface HashTagRepository extends JpaRepository<HashTag, Integer> {
     //태그 찾기, 태그 등록, 태그 삭제, 태그 수정
     Optional<HashTag> findByTagName(String tagName);
 
+    List<HashTag> findAll();
+
+    Optional<HashTag> findByIdx(int idx);
+
 
     //태그 등록 , 삭제, 수정은 (CRUD)  Spring Data JPA에서는 JpaRepository를 상속받은 인터페이스를 사용하면,
     // 기본적인 CRUD 기능이 자동으로 제공된다.
 
 
-    // 추가적으로 필요한 메서드가 있다면 여기에 추가할 수 있습니다.
+
 }
--- backend/src/main/java/com/toki/backend/hashTag/service/HashTagService.java
+++ backend/src/main/java/com/toki/backend/hashTag/service/HashTagService.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class HashTagService {
 
     private HashTagRepository hashTagRepository;
-
+    //태그 찾기, 태그 추가, 태그 수정, 태그 삭제 4개정도
 
     // 태그 찾기
     public List<HashTagDTO> findAllHashTags() {
@@ -24,4 +24,18 @@ public class HashTagService {
                 .map(tag -> convertEntityToDto(tag))
                 .collect(Collectors.toList());
     }
+
+    // 태그 추가
+    public HashTagDTO addHashTag(HashTagDTO hashTagDTO) {
+        HashTag newTag = new HashTag(
+
+                hashTagDTO.getTagName(),
+                hashTagDTO.getCreatedAt(),
+                hashTagDTO.getScore(),
+                hashTagDTO.getLastUsedAt()
+        );
+        HashTag savedTag = hashTagRepository.save(newTag);
+        return convertEntityToDto(savedTag);
+    }
+
 }
