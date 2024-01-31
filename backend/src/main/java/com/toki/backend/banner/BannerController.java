8543aa655b027b9ecb386acf6812103cc5fa29cc
geunhyeongKim <ppoyam_2@naver.com>
2024-01-31 17:21:28 +0900

    refactor:Banner->CommonDTO로수정하였음240131

--- backend/src/main/java/com/toki/backend/banner/BannerController.java
+++ backend/src/main/java/com/toki/backend/banner/BannerController.java
@@ -6,6 +6,8 @@ import com.toki.backend.banner.dto.BannerDTO;
 import com.toki.backend.banner.entity.Banner;
 import com.toki.backend.banner.repository.BannerRepository;
 import com.toki.backend.banner.service.BannerService;
+import com.toki.backend.common.dto.response.CommonResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,53 +18,48 @@ import java.util.List;
 import java.util.Optional;
 
 
+
 @RestController
-@RequestMapping("/api/v1/banner")
+@RequestMapping("/api/banners")
+@RequiredArgsConstructor
 public class BannerController {//1. 배너 전체 조회  2. 특정 배너 조회, 3. 삭제 4. 추가. 5.수정
 
 
-    @Autowired
+
     private BannerService bannerService;
 
 
 
-    //1. 배너 전체를 조회하기
+    // 모든 배너 조회
     @GetMapping
-    public ResponseEntity<List<BannerDTO>> getAllBanners() {
-        List<BannerDTO> banners = bannerService.getAllBanners();
-        return new ResponseEntity<>(banners, HttpStatus.OK);
+    public CommonResponseDto<List<BannerDTO>> getAllBanners() {
+        return bannerService.getAllBanners();
     }
 
 
-    //2. 특정 배너를 조회하기
+    // 특정 인덱스의 배너 조회
     @GetMapping("/{idx}")
-    public ResponseEntity<?> getBannerByIdx(@PathVariable int idx) {
-        List<?> bannerNames = bannerService.findByIdx(idx);
-        return bannerNames.isEmpty() ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
-                : new ResponseEntity<>(bannerNames, HttpStatus.OK);
+    public CommonResponseDto<BannerDTO> getBannerByIdx(@PathVariable int idx) {
+        return bannerService.findByIdx(idx);
     }
 
-    //3. 삭제
+
+    // 특정 인덱스의 배너 삭제
     @DeleteMapping("/{idx}")
-    public ResponseEntity<?> deleteBanner(@PathVariable int idx) {
-        bannerService.deleteBanner(idx);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public CommonResponseDto<Void> deleteBanner(@PathVariable int idx) {
+        return bannerService.deleteBanner(idx);
     }
 
-    //4. 추가
-    //
+    // 새로운 배너 추가
     @PostMapping
-    public ResponseEntity<BannerDTO> addBanner(@RequestBody BannerDTO bannerDTO) {
-        BannerDTO addedBanner = bannerService.addBanner(bannerDTO);
-        return new ResponseEntity<>(addedBanner, HttpStatus.CREATED);
+    public CommonResponseDto<BannerDTO> addBanner(@RequestBody BannerDTO bannerDto) {
+        return bannerService.addBanner(bannerDto);
     }
 
 
-    //5. 수정
+    // 특정 인덱스의 배너 수정
     @PutMapping("/{idx}")
-    public ResponseEntity<BannerDTO> updateBanner(@PathVariable int idx, @RequestBody BannerDTO updatedBannerDTO) {
-        BannerDTO updatedBanner = bannerService.updateBanner(idx, updatedBannerDTO);
-        return updatedBanner != null ? new ResponseEntity<>(updatedBanner, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public CommonResponseDto<BannerDTO> updateBanner(@PathVariable int idx, @RequestBody BannerDTO updatedBannerDTO) {
+        return bannerService.updateBanner(idx, updatedBannerDTO);
     }
 }
--- backend/src/main/java/com/toki/backend/banner/service/BannerService.java
+++ backend/src/main/java/com/toki/backend/banner/service/BannerService.java
@@ -1,13 +1,13 @@
 package com.toki.backend.banner.service;
 
+
 import com.toki.backend.banner.dto.BannerDTO;
 import com.toki.backend.banner.entity.Banner;
 import com.toki.backend.banner.repository.BannerRepository;
+import com.toki.backend.common.dto.response.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,48 +17,117 @@ public class BannerService {
 
     private final BannerRepository bannerRepository;
 
-
-
     // 1. 모두 조회
-    public List<BannerDTO> getAllBanners() {
-        List<Banner> banners = bannerRepository.findAll();
-        return banners.stream()
-                .map(this::convertEntityToDto)
-                .collect(Collectors.toList());
-    }
+    public CommonResponseDto<List<BannerDTO>> getAllBanners() {
+        try {
+            List<Banner> banners = bannerRepository.findAll();
+            List<BannerDTO> bannerDTO = banners.stream()
+                    .map(this::convertEntityToDto)
+                    .collect(Collectors.toList());
 
+            return CommonResponseDto.<List<BannerDTO>>builder() //성공한 경우
+                    .resultCode(200)
+                    .resultMessage("조회에 성공하였습니다.")
+                    .data(bannerDTO)
+                    .build();
+        } catch (Exception e) { //실패한 경우
+            return CommonResponseDto.<List<BannerDTO>>builder()
+                    .resultCode(400)
+                    .resultMessage("조회에 실패하였습니다.")
+                    .build();
+        }
+    }
 
     // 2. 특정 인덱스만 조회
-    public List<?> findByIdx(int idx) {
-        Banner banner = bannerRepository.findByIdx(idx).orElse(null);
-        return banner != null ? Collections.singletonList(banner.getName()) : Collections.emptyList();
-    }
+    public CommonResponseDto<BannerDTO> findByIdx(int idx) {
+        try {
+            Banner banner = bannerRepository.findByIdx(idx).orElse(null); //null값 처리하기 위함
 
+            if (banner != null) {
+                BannerDTO bannerDTO = convertEntityToDto(banner);
+                return CommonResponseDto.<BannerDTO>builder()
+                        .resultCode(200)
+                        .resultMessage("조회에 성공하였습니다.")
+                        .data(bannerDTO)
+                        .build();
+            } else {
+                return CommonResponseDto.<BannerDTO>builder()
+                        .resultCode(404)
+                        .resultMessage("찾을 수 없는 배너입니다.")
+                        .build();
+            }
+        } catch (Exception e) {
+
+            return CommonResponseDto.<BannerDTO>builder()
+                    .resultCode(400)
+                    .resultMessage("조회에 실패하였습니다.")
+                    .build();
+        }
+    }
 
     // 3. 삭제
-    public void deleteBanner(int idx) {
-        bannerRepository.deleteById(idx);
+    public CommonResponseDto<Void> deleteBanner(int idx) {
+        try {
+            bannerRepository.deleteById(idx);
+            return CommonResponseDto.<Void>builder()
+                    .resultCode(200)
+                    .resultMessage("삭제에 성공하였습니다.")
+                    .build();
+        } catch (Exception e) {
+            // 예외 처리 또는 로깅 추가
+            return CommonResponseDto.<Void>builder()
+                    .resultCode(400)
+                    .resultMessage("삭제에 실패하였습니다.")
+                    .build();
+        }
     }
 
 
-    //4. 추가
-    public BannerDTO addBanner(BannerDTO bannerDto) {
-        Banner newBanner = new Banner(bannerDto.getIdx(), bannerDto.getName(), bannerDto.getImageUrl());
-        Banner savedBanner = bannerRepository.save(newBanner);
-        return new BannerDTO(savedBanner.getIdx(), savedBanner.getName(), savedBanner.getImageUrl());
+    // 4. 추가
+    public CommonResponseDto<BannerDTO> addBanner(BannerDTO bannerDto) {
+        try {
+            Banner newBanner = new Banner(bannerDto.getIdx(), bannerDto.getName(), bannerDto.getImageUrl());
+            Banner savedBanner = bannerRepository.save(newBanner);
+
+            return CommonResponseDto.<BannerDTO>builder()
+                    .resultCode(200)
+                    .resultMessage("베너 추가에 성공하였습니다.")
+                    .data(convertEntityToDto(savedBanner))
+                    .build();
+        } catch (Exception e) {
+            return CommonResponseDto.<BannerDTO>builder()
+                    .resultCode(400)
+                    .resultMessage("배너 추가에 실패하였습니다.")
+                    .build();
+        }
     }
-    //5. 수정
-    public BannerDTO updateBanner(int idx, BannerDTO updatedBannerDTO) {
-        Banner bannerToUpdate = bannerRepository.findByIdx(idx).orElse(null);
 
-        if (bannerToUpdate != null) {
-            bannerToUpdate.setName(updatedBannerDTO.getName());
-            bannerToUpdate.setImageUrl(updatedBannerDTO.getImageUrl());
+    // 5. 수정
+    public CommonResponseDto<BannerDTO> updateBanner(int idx, BannerDTO updatedBannerDTO) {
+        try {
+            Banner bannerToUpdate = bannerRepository.findByIdx(idx).orElse(null);
 
-            Banner updatedBanner = bannerRepository.save(bannerToUpdate);
-            return convertEntityToDto(updatedBanner);
-        } else {
-            return null;
+            if (bannerToUpdate != null) {
+                bannerToUpdate.setName(updatedBannerDTO.getName());
+                bannerToUpdate.setImageUrl(updatedBannerDTO.getImageUrl());
+
+                Banner updatedBanner = bannerRepository.save(bannerToUpdate);
+                return CommonResponseDto.<BannerDTO>builder()
+                        .resultCode(200)
+                        .resultMessage("배너 수정에 성공하였습니다.")
+                        .data(convertEntityToDto(updatedBanner))
+                        .build();
+            } else {
+                return CommonResponseDto.<BannerDTO>builder()
+                        .resultCode(400)
+                        .resultMessage("배너를 찾을 수 없습니다.")
+                        .build();
+            }
+        } catch (Exception e) {
+            return CommonResponseDto.<BannerDTO>builder()
+                    .resultCode(400)
+                    .resultMessage("배너를 수정할 수 없습니다.")
+                    .build();
         }
     }
 
