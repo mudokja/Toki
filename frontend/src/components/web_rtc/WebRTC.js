f4dfb3147de7e4d882cc41db8af5e16d7937bd41
백현우 <ekgoddlek7@naver.com>
2024-02-05 09:23:24 +0900

    feat:Test WebRTC Display2

--- frontend/src/components/web_rtc/WebRTC.js
+++ frontend/src/components/web_rtc/WebRTC.js
@@ -5,7 +5,7 @@ import adapter from 'webrtc-adapter';
 adapter
 var participants = {};
 var name;
-const ws = new WebSocket('wss://localhost:8443/groupcall');
+const ws = new WebSocket('wss://i10b205.p.ssafy.io/ws/room');
 window.onbeforeunload = function() {
 	ws.close();
 };
