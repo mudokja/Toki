4a4e86cd56ae787e62bd47da6714ee2c8507933b
geunhyeongKim <ppoyam_2@naver.com>
2024-02-06 15:39:04 +0900

    ConvertUserTag_Method

--- backend/src/main/java/com/toki/backend/common/utils/ConvertUserTag.java
+++ backend/src/main/java/com/toki/backend/common/utils/ConvertUserTag.java
@@ -3,8 +3,7 @@ package com.toki.backend.common.utils;
 public class ConvertUserTag {
 
     // 유저 태그를 16진수로 변환하고 다시 10진수로 변환하는 메서드
-    /* toHex가 true이면 10진수를 16진수로 변환
-       toHex가 false이면 16진수를 10진수로 변환 */
+
     public String convertUserTag(int userTag) {
         return Integer.toHexString(userTag);
     }
