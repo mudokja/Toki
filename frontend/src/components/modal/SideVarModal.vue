b66e602fe55e33910eae98742af071f713f96b1a
백현우 <ekgoddlek7@naver.com>
2024-02-16 14:08:18 +0900

    fix:profile2

--- frontend/src/components/modal/SideVarModal.vue
+++ frontend/src/components/modal/SideVarModal.vue
@@ -79,7 +79,7 @@ const storeAuth = useAuthStore()
       </v-btn>
 
 
-      <RouterLink v-if="storeAuth.userTag === null || storeAuth.userTag === undefined" :to="{ name: 'home'}">
+      <RouterLink  :to="{ name: 'home'}">
         <img src="@/assets/profile_assets/프로필.png" style="width: 40px; height: 40px; border-radius: 50%; margin-left: 1px;">
         <div>
             <h4 style="margin-left: 10px;">5반 유저</h4>
